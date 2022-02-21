c03b40452eb294ecf9a1c6d38bab56e5d4d09e6f
Robin <robinx0852@gmail.com>
2022-02-21 21:30:48 +0200

    Better error handling messages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,35 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import {generateBoard} from './services/game';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { generateBoard } from "./services/game";
 
 admin.initializeApp();
 
-export const generateGame = functions.https.onRequest(async (request, response) => {
-  const {size, mines} = request.body;
+export const generateGame = functions.https.onRequest(
+  async (request, response) => {
+    const { size, mines } = request.body;
 
-  if (size >= 4 && mines >= 1) {
-    const result = await admin.firestore()
-        .collection('games')
+    if (size >= 4 && mines >= 1) {
+      const result = await admin
+        .firestore()
+        .collection("games")
         .add(generateBoard(size, mines));
-    response.status(200).send(result.id);
+      response.status(200).send(result.id);
+    }
+    functions.logger.error("Unable to create game", request.body);
+    response
+      .status(400)
+      .send(
+        "Something terrible has happened, please provide appropriate size and mines."
+      );
   }
-  response.status(400).send('size >= 4 && mines >= 1');
-});
+);
 
-export const createNewUser = functions.auth.user().onCreate(async (user, context) => {
-  await admin.firestore()
-      .collection('users')
+export const createNewUser = functions.auth
+  .user()
+  .onCreate(async (user, context) => {
+    admin
+      .firestore()
+      .collection("users")
       .doc(user.uid)
       .create({
         name: user.displayName,
@@ -27,6 +38,6 @@ export const createNewUser = functions.auth.user().onCreate(async (user, context
         uid: user.uid,
       })
       .catch((reason) => {
-        functions.logger.error('Terrible level, could not create new user', user, context, reason);
+        functions.logger.error("Unable to create user.", user, context, reason);
       });
-});
+  });
